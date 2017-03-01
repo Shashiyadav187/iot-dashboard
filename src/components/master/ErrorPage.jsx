ecc9d24e6d8f919922df5b03c37708b22d9b9b83
ypereirars <ypereira@gmail.com>
2017-03-01 02:53:03 -0300

    Scaffolds basic components.

---
+++ src/components/master/ErrorPage.jsx
@@ -0,0 +1,27 @@
+'use strict'
+
+import React from 'react'
+
+export default class ErrorPage extends React.Component {
+  render() {
+    const error = this.props.error; 
+    const stackTrace = (typeof error !== 'undefined') ? 
+                  <div>
+                     <h2>Error code: {error.code}</h2>
+                     <h3>Stack Trace:</h3>
+                     <p>{error.stack}</p>
+                   </div> :
+                   ''
+                                  
+    return (
+      <div className="not-found">
+        <h1>404</h1>
+        <h2>Page Not Found!</h2>
+        <p>
+          <span>Stack trace</span>
+          {stackTrace}
+        </p>
+      </div>
+    )
+  }
+}
---
+++ src/components/master/IndexPage.jsx
@@ -0,0 +1,11 @@
+'use strict'
+
+import React from 'react'
+
+export default class IndexPage extends React.Component {
+  render() {
+    return (
+      <div>React scaffold</div>
+    )
+  }
+}
